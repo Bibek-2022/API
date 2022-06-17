2dedb15175077a61730a9b41e15e265235f656b1
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-17 22:50:42 +1000

    added url and rearranged the functions

--- index.js
+++ index.js
@@ -5,28 +5,96 @@ const cheerio = require("cheerio");
 
 const app = express();
 const articles = [];
+const newspapers = [
+  {
+    name: "cityam",
+    address:
+      "https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/",
+    base: "",
+  },
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+    base: "https://www.telegraph.co.uk",
+  },
+  {
+    name: "nyt",
+    address: "https://www.nytimes.com/international/section/climate",
+    base: "",
+  },
+  {
+    name: "latimes",
+    address: "https://www.latimes.com/environment",
+    base: "",
+  },
+  {
+    name: "smh",
+    address: "https://www.smh.com.au/environment/climate-change",
+    base: "https://www.smh.com.au",
+  },
+  {
+    name: "un",
+    address: "https://www.un.org/climatechange",
+    base: "",
+  },
+  {
+    name: "bbc",
+    address: "https://www.bbc.co.uk/news/science_and_environment",
+    base: "https://www.bbc.co.uk",
+  },
+  {
+    name: "es",
+    address: "https://www.standard.co.uk/topic/climate-change",
+    base: "https://www.standard.co.uk",
+  },
+  {
+    name: "sun",
+    address: "https://www.thesun.co.uk/topic/climate-change-environment/",
+    base: "",
+  },
+  {
+    name: "dm",
+    address:
+      "https://www.dailymail.co.uk/news/climate_change_global_warming/index.html",
+    base: "",
+  },
+  {
+    name: "nyp",
+    address: "https://nypost.com/tag/climate-change/",
+    base: "",
+  },
+];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    $('a:contains("politics")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      articles.push({
+        title,
+        url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
 app.get("/", (req, res) => {
   res.json("Welcome to my 1st api");
 });
 app.get("/news", (res, req) => {
-  axios
-    .get("https://www.abc.net.au/news/politics")
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const specificArticles = [];
-
-      $('a:contains("politics")', html).each(function () {
-        const title = $(this).text();
-        const url = $(this).attr("href");
-        articles.push({
-          title,
-          url: newspaperBase + url,
-          source: newspaperId,
-        });
-      });
-      res.json(articles);
-    })
-    .catch((err) => console.log(err));
+  res.json(articles);
 });
 app.listen(PORT, () => console.log("server "));
